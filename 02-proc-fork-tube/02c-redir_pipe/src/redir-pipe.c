c458e959bb9cdd5ca70fe13f5107d0c2cd6022c1
Fabien Vantard <f.vantard@gmail.com>
2017-04-04 17:36:08 +0200

    Another commit.

--- 02-proc-fork-tube/02c-redir_pipe/src/redir-pipe.c
+++ 02-proc-fork-tube/02c-redir_pipe/src/redir-pipe.c
@@ -13,6 +13,7 @@
 #include <libgen.h>
 #include <limits.h>
 
+#define STDIN  0
 #define STDOUT 1
 #define STDERR 2
 
@@ -21,29 +22,79 @@
         ps eaux | grep "^root " > /dev/null && echo "root est connecté"
 
     Votre programme devra ainsi prendre en charge les actions suivantes :
-        o Réaliser les exécutions de ps et grep en utilisant la primitive execlp dans l’ordre des priorités usuelles.
-        o Mettre en place les redirections des entrées/sorties nécessaires grâce à la primitive dup (ou dup2).
-        o Réaliser l’affichage final avec la primitive write.
+      o Réaliser les exécutions de ps et grep en utilisant la primitive execlp dans l’ordre des priorités usuelles.
+      o Mettre en place les redirections des entrées/sorties nécessaires grâce à la primitive dup (ou dup2).
+      o Réaliser l’affichage final avec la primitive write.
 
     Vous chercherez à simplifier le code pour ne prendre en charge que cette séquence de commandes.
 */
 
 
-
 int main(int argc, char** argv)
 {
-    if (execlp("ps", "eaux", (char *)NULL) == -1) {
-        perror("execlp");
-        exit(EXIT_FAILURE);
-    }
+	int pipefd[2];
+	pid_t parent, child1, child2;
 
-    if (execlp("grep", "\"^root \"", (char *)NULL) == -1) {
-        perror("execlp");
-        exit(EXIT_FAILURE);
-    }
+	parent = getpid();
 
-    // FIXME
+	printf("[PARENT-%d] Trying to create a pipe...\n", parent);
+	if (pipe(pipefd) <0) {
+		perror("pipe");
+		dprintf(STDERR, "error: can't create a pipe.\n");
+		exit(EXIT_FAILURE);
+	}
+	printf("[PARENT-%d] Pipe created: pipefd  = { [0]=>%d , [1]=>%d }.\n", parent, pipefd[0], pipefd[1]);
 
+	child1 = fork();
+	if (child1 == 0) {
+		close(pipefd[0]);
+		dup2(pipefd[1], STDOUT);
+		//execlp("/bin/ls", "ls", "-l", "/tmp", (char *)NULL);
+		execlp("/bin/ps", "ps", "eaux", (char *)NULL);
+	}
 
-    return EXIT_SUCCESS;
+	child2 = fork();
+	if (child2 == 0) {
+		close(pipefd[1]);
+		dup2(pipefd[0], STDIN);
+		//execlp("tr", "tr", "a-z", "A-Z", (char *)NULL);
+		execlp("grep", "grep", "\"root\"", (char *)NULL);
+	}
+
+	close(pipefd[0]);
+	close(pipefd[1]);
+
+	waitpid(child2, NULL, 0);
+	//close(pipefd[1]);
+	//dup2(pipefd[0], STDIN);
+	//close(pipefd[0]);
+
+	return EXIT_SUCCESS;
 }
+
+
+
+/*
+perror("fork"); dprintf(STDERR, "error: can't fork the program.\n"); exit(EXIT_FAILURE);
+perror("fork"); dprintf(STDERR, "error: can't fork the program.\n"); exit(EXIT_FAILURE);
+printf("[CHILD#1-%d] Duping [%d] to [%d] ...", selfpid, pipefd[1], STDOUT);
+printf("[CHILD#2-%d] Duping [%d] to [%d] ...", selfpid, pipefd[0], STDIN);
+if (dup2(pipefd[1], STDOUT) <0) {
+perror("dup2");
+dprintf(STDERR, "error: dup2 - can't dup [%d] to [%d]\n", pipefd[1], STDOUT);
+exit(EXIT_FAILURE);
+}
+
+if (dup2(pipefd[0], STDIN) <0) {
+perror("dup2");
+dprintf(STDERR, "error: dup2 - can't dup [%d] to [%d]\n", pipefd[0], STDIN);
+exit(EXIT_FAILURE);
+}
+
+int cr = -1;
+close(pipefd[0]);
+close(pipefd[1]);
+printf("[PARENT-%d] Waiting...\n", parent);
+wait(&cr);
+printf("[PARENT-%d] Wait ends.\n", parent);
+*/
