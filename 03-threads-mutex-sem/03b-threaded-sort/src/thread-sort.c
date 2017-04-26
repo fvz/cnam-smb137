6460fa5a65b8b064bf5f11f2a34a92dfab11dff8
Fabien Vantard <f.vantard@gmail.com>
2017-04-26 17:30:16 +0200

    Arguments in command line and Interactive mode.

--- 03-threads-mutex-sem/03b-threaded-sort/src/thread-sort.c
+++ 03-threads-mutex-sem/03b-threaded-sort/src/thread-sort.c
@@ -14,8 +14,9 @@
 #include <limits.h>
 #include <time.h>
 #include <pthread.h>
+#include <getopt.h>
 
-/*
+/**E*N*O*N*C*E***E*X*E*R*C*I*C*E**********************************************
    Ecrivez un programme qui :
    - initialise un grand tableau d’entiers avec des valeurs aleatoires,
    - recherche ensuite le minimum et le maximum du tableau et affiche le resultat.
@@ -38,11 +39,17 @@
    machine reel alors que clock donne la somme des temps d’execution des threads (principal et fils).
 
    Remarque 2 : pour la phase de compilation ne pas oublier d'utiliser l'option -lpthread avec gcc.
+*****************************************************************************/
 
-   TODO : proposer la saisie (fonction scanf) de la taille tableau + nb thread
-*/
 
+/* dynamic array prefered for user defined parameter (via getopt / command line) */
 int *tab = NULL;
+
+/* default values */
+#define DEFAULT_ARRAY_SIZE	((int)1e8)
+#define DEFAULT_THREADS_NUMBER	(4)
+
+#define BUFFER_SIZE 1024
 
 
 typedef struct resultat_s {
@@ -68,9 +75,8 @@ void *search_min_max(void *arg)
 		if (tab[i] < t->r.min) { t->r.min = tab[i]; }
 		if (tab[i] > t->r.max) { t->r.max = tab[i]; }
 
-		/* just for debug */
 		#if 0
-		if (i%10000 == 0) {
+		if (i%10000 == 0) { /* just for debug */
 			printf("thread[%li] i=[%d]\n", (unsigned long int) t->th, i);
 		}
 		#endif
@@ -78,40 +84,120 @@ void *search_min_max(void *arg)
 	pthread_exit(NULL);
 }
 
+
+#define USAGE_SYNTAX "[OPTIONS]"
+#define USAGE_PARAMS "OPTIONS:\n\
+  -a, --array  LENGTH_OF_ARRAY    : length of random array.\n\
+  -t, --thread NUMBER_OF_THREADS  : number of threads to create.\n\
+***\n\
+  -i, --interactive : enable *interactive* mode\n\
+  -v, --verbose     : enable *verbose* mode\n\
+  -h, --help        : display this help\n\
+"
+
+void print_usage(char* bin_name)
+{
+	printf("USAGE: %s %s\n\n%s\n", bin_name, USAGE_SYNTAX, USAGE_PARAMS);
+}
+
+struct option binary_opts[] =
+{
+	{ "help",	no_argument,		0,	'h' },
+	{ "verbose",	no_argument,		0,	'v' },
+	{ "interactive",no_argument,		0,	'i' },
+	{ "array",	required_argument,	0,	'a' },
+	{ "thread",	required_argument,	0,	't' },
+	{ 0,		0,			0,	0 }
+};
+const char* binary_optstr = "hvia:t:";
+
+
 int main(int argc, char** argv)
 {
+	int is_verbose_mode = 0;
+	int is_interactive_mode = 0;
 	int i, len;
+	int ith, nthread;
 	struct timeval t1, t2;
 	resultat_t r;
-	int ith, nthread = 4;
 	mythread_t *mythreads;
 
-	#define SIZE (5*((int)1e8))
+	/* use default value if not specified in args or in interactive mode */
+	len = DEFAULT_ARRAY_SIZE;
+	nthread = DEFAULT_THREADS_NUMBER;
 
-	len = SIZE;
+	/* parsing options */
+	int opt = -1;
+	int opt_idx = -1;
 
-	printf("mallocing array of [%d] int elements... ", len);
-	if (!(tab = malloc (len * sizeof(int)))) {
-		printf("[NOK]\n");
-		perror("malloc array of int.");
-		return EXIT_SUCCESS;
+	while ((opt = getopt_long(argc, argv, binary_optstr, binary_opts, &opt_idx)) != -1)
+	{
+		switch (opt)
+		{
+			case 'a':
+				if (optarg) { len = atoi(optarg); }
+				break;
+			case 't':
+				if (optarg) { nthread = atoi(optarg); }
+				break;
+			case 'i':
+				is_interactive_mode = 1;
+				break;
+			case 'v':
+				is_verbose_mode = 1;
+				break;
+			case 'h':
+				print_usage(argv[0]);
+				exit(EXIT_SUCCESS);
+			default :
+				break;
+		}
 	}
-	printf("[OK]\n");
 
-	printf("Number of threads [%d]: ", nthread);
-	// FIXME : si vide (enter), prendre valeur par defaut
-	scanf("%d", &nthread);
+	/* interactive mode : scanning the stdin for retrieving values parameters */
+	if (is_interactive_mode) {
 
-	//printf("[notice] const: RAND_MAX=[%u] INT_MAX=[%u] UINT_MAX=[%u]\n", RAND_MAX, INT_MAX, UINT_MAX);
-	//printf("[notice] sizeof(tab)=[%ld]  size(tab)=[%d elements]\n", sizeof(tab), len);
+		char buffer[BUFFER_SIZE];
 
+		printf("Length of array [%d]: ", len);
+		if (fgets(buffer, BUFFER_SIZE, stdin) != NULL) {
+			char *pos = NULL;
+			if ((pos = strchr(buffer, '\n'))) { *pos = '\0'; }
+			if (strlen(buffer) > 0) { len = atoi(buffer); }
+		}
+
+		printf("Number of threads [%d]: ", nthread);
+		if (fgets(buffer, BUFFER_SIZE, stdin) != NULL) {
+			char *pos = NULL;
+			if ((pos = strchr(buffer, '\n'))) { *pos = '\0'; }
+			if (strlen(buffer) > 0) { nthread = atoi(buffer); }
+		}
+	}
+
+
+	/* check len/nthread values (because atoi() can return 0 if string isn't a number) */
+	if (len <= 0) { len = DEFAULT_ARRAY_SIZE; }
+	if (nthread <= 0) { nthread = DEFAULT_THREADS_NUMBER; }
+
+
+	/* allocating array of 'len' int elements */
+	if (is_verbose_mode) {	printf("mallocing array of [%d] int elements... ", len); }
+	if (!(tab = malloc (len * sizeof(int)))) {
+		if (is_verbose_mode) { printf("[NOK]\n"); }
+		perror("malloc array of int.");
+		return EXIT_FAILURE;
+	}
+	if (is_verbose_mode) { printf("[OK]\n"); }
+
+
+	/* initialization of array with random values */
 	srand(time(NULL));
-	printf("initialization 'tab' (array of %d elements) ... ", len);
+	if (is_verbose_mode) { printf("initialization 'tab' (array of %d elements) ... ", len); }
 	for (i=0; i<len; i++) { tab[i] = rand(); }
-	printf("[OK]\n");
+	if (is_verbose_mode) { printf("[OK]\n"); }
 
 
-/*** WITHOUT THREAD ****
+	/*** WITHOUT THREAD **************************************************
 	r.min = r.max = tab[0];
 	r.b = 0; r.e = len;
 
@@ -124,10 +210,18 @@ int main(int argc, char** argv)
 	}
 	gettimeofday(&t2, NULL);
 	printf("[OK]\n");
-***/
+	*********************************************************************/
 
 
-	mythreads = (mythread_t *) malloc (nthread * sizeof(mythread_t));
+	/* allocating array of 'nthread' elements for handling the N threads */
+	if (!(mythreads = (mythread_t *) malloc (nthread * sizeof(mythread_t)))) {
+		perror("malloc array of threads.");
+		free(tab);
+		return EXIT_FAILURE;
+	}
+
+
+	/* start time counter */
 	gettimeofday(&t1, NULL);
 
 	i=0;
@@ -146,7 +240,9 @@ int main(int argc, char** argv)
 			return EXIT_FAILURE;
 		}
 
-		printf("thread[%3d] id=[%li] created.\n", ith, (unsigned long int) mythreads[ith].th);
+		if (is_verbose_mode) {
+			printf("thread[%3d] id=[%li] created.\n", ith, (unsigned long int) mythreads[ith].th);
+		}
 	}
 
 	/* join of all N threads */
@@ -158,13 +254,14 @@ int main(int argc, char** argv)
 	}
 
 	/* display of min/max found by each threads */
-	for(ith=0; ith < nthread; ith++) {
-		printf("thread[%3d]: min=[%10d] max=[%10d]\n",
-			ith, mythreads[ith].r.min, mythreads[ith].r.max);
+	if (is_verbose_mode) {
+		for(ith=0; ith < nthread; ith++) {
+			printf("thread[%3d]: min=[%10d] max=[%10d]\n",
+				ith, mythreads[ith].r.min, mythreads[ith].r.max);
+		}
 	}
 
-
-	/* search final min/max between the min/max of all threads */
+	/* search final min/max values between the min/max of all threads */
 	r.min = mythreads[0].r.min;
 	r.max = mythreads[0].r.max;
 	for(ith=1; ith < nthread; ith++) {
@@ -172,12 +269,15 @@ int main(int argc, char** argv)
 		if (mythreads[ith].r.max > r.max) { r.max = mythreads[ith].r.max; }
 	}
 
+	/* stop time counter */
 	gettimeofday(&t2, NULL);
+
+	/* freeing allocated arrays */
 	free (mythreads);
 	free (tab);
 
-	printf("END: min=[%d] max=[%d]\n", r.min, r.max);
 
+	printf("Scanning Array[%d] / With [%d] Threads / Give min/max [%d/%d] values", len, nthread, r.min, r.max);
 
 	/* time duration in minutes, seconds, microseconds */
 	long int diff_usec, diff_sec, diff_min;
@@ -188,7 +288,7 @@ int main(int argc, char** argv)
 	diff_min = diff_sec/60;
 	diff_sec %= 60;
 
-	printf(" |> elapsed:\t%ldm%ld.%03lds\n", diff_min, diff_sec, diff_usec/1000);
+	printf(" / In Elapsed Time [%ldm%ld.%03lds]\n", diff_min, diff_sec, diff_usec/1000);
 
 	return EXIT_SUCCESS;
 }
