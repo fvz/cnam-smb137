a9d9775c4d75423bb1282620beaea30b84981786
Fabien Vantard <f.vantard@gmail.com>
2017-06-14 20:38:30 +0000

    Add strcat_dup()

--- 00-prj-mini-shell/include/facilities.h
+++ 00-prj-mini-shell/include/facilities.h
@@ -7,6 +7,6 @@
 
 
 void freeif(void* to_free);
-
+char* strcat_dup(const char *s1, const char *s2);
 
 #endif /* MYSH_FACILITIES_H_ */
--- 00-prj-mini-shell/src/facilities.c
+++ 00-prj-mini-shell/src/facilities.c
@@ -20,3 +20,22 @@ void freeif(void* to_free)
 {
     if (to_free != NULL) free(to_free);
 }
+
+/**
+ * \fn char* strcat_dup(const char *s1, const char *s2)
+ * \brief concatène 2 chaines et duplique en mémoire
+ *
+ * \param s1 chaine n°1
+ * \param s2 chaine n°1
+ * \return nouvelle chaine allouée contenant la concaténation de s1 et s2.
+ */
+char* strcat_dup(const char *s1, const char *s2) {
+    int len1, len2;
+    char *new;
+    len1 = str1 ? strlen(s1) : 0;
+    len2 = str2 ? strlen(s2) : 0;
+    new = malloc(len1 + len2 + 1); *new = '\0';
+    if (s1) { memcpy(new,        s1, len1 + 1); }
+    if (s2) { memcpy(new + len1, s2, len2 + 1); }
+    return new;
+}
