8553261149e8603c6097365c9d51c6b0903686a8
Fabien Vantard <f.vantard@gmail.com>
2017-06-15 00:09:24 +0000

    Add code of fork example..

--- 00-prj-mini-shell/src/cmdfork.c
+++ 00-prj-mini-shell/src/cmdfork.c
@@ -25,11 +25,13 @@ int cmdfork_do(mysh_context_p ctx, cmdredir_p r) {
 
     ppid = getpid();
     child = fork();
-    if (child == 0) {
+
+    if (child == 0) { /* executed by child */
         ctx_dbmyprintf(1, ctx, M_CMDFORK_INCHILD_WORK_EXEC, getpid(), r->cmd);
         execvp(r->args[0], r->args);
         //perror("execlp");
-    } else {
+
+    } else { /* executed by parent */
         ctx_dbmyprintf(1, ctx, M_CMDFORK_FORK_TO_CHILD_WAIT, ppid, child);
         waitpid(child, &status, 0);
         ctx_dbmyprintf(1, ctx, M_CMDFORK_END_OF_WAIT, ppid, child, WEXITSTATUS(status));
@@ -39,6 +41,60 @@ int cmdfork_do(mysh_context_p ctx, cmdredir_p r) {
 
     return true;
 }
+
+
+
+#if 0
+int
+main(int argc, char *argv[])
+{
+    pid_t cpid, w;
+    int status;
+
+   cpid = fork();
+    if (cpid == -1) {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    }
+
+   if (cpid == 0) {            /* Code executed by child */
+        printf("Child PID is %ld\n", (long) getpid());
+        if (argc == 1)
+            pause();                    /* Wait for signals */
+        _exit(atoi(argv[1]));
+
+   } else {                    /* Code executed by parent */
+        do {
+            w = waitpid(cpid, &status, WUNTRACED | WCONTINUED);
+            if (w == -1) {
+                perror("waitpid");
+                exit(EXIT_FAILURE);
+            }
+
+           if (WIFEXITED(status)) {
+                printf("exited, status=%d\n", WEXITSTATUS(status));
+            } else if (WIFSIGNALED(status)) {
+                printf("killed by signal %d\n", WTERMSIG(status));
+            } else if (WIFSTOPPED(status)) {
+                printf("stopped by signal %d\n", WSTOPSIG(status));
+            } else if (WIFCONTINUED(status)) {
+                printf("continued\n");
+            }
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+        exit(EXIT_SUCCESS);
+    }
+}
+#endif
+
+
+
+
+
+
+
+
+
+
 
 
 /*
